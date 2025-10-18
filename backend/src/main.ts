e4de65f7b9849f262da5eef257ef0b9d7898a80f
Ematius <eksaber87@gmail.com>
2025-10-18 13:26:41 +0200

    enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [
+      'http://localhost:4200',
+      'https://frontend-production-f433.up.railway.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
--- frontend/src/environments/environment.development.ts
+++
@@ -1,1 +0,0 @@
-export const environment = { apiBase: 'http://localhost:3000' };
